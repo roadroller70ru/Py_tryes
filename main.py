b235ce08d7667166811769ffe0647f98eb607fe6
breareros <breareros@gmail.com>
2022-02-08 11:21:56 +0500

    as function

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #C=5/9*(F-32)
-ftemp = float(input("Input Fahrenheit temp: "))
-ctemp = 5.0/9*(ftemp - 32)
-print("Celsius temp is: ", ctemp, end='')
+def F2C():
+    '''Input Fahrenheit temp and return Celsius temp'''
+    ftemp = float(input("Input Fahrenheit temp: "))
+    ctemp = 5.0/9*(ftemp - 32)
+    return(ctemp)
+
+
+
+def main():
+    print(f"Celsius temp is: {F2C()}")
+
+if __name__ == '__main__':
+    main()
